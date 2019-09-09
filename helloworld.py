8e05057087fc7ef3bb6429ea3b30275a3b4724be
nbelans <nbelans@umich.edu>
2019-09-09 14:39:36 -0400

    Lab 0 Hello World File

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+from random import randint
+int1 = randint(1, 100)
+int2 = randint(1, 100)
+sum = int1 + int2
+average = sum / 2.0
+
+print("Nicole Belans")
+print(int1)
+print(int2)
+print("Sum = " + str(sum))
+print("Average = " + str(average))
